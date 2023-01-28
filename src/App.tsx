b9e37fc2212e2a95690516b0e1dede4f06a5bf08
bmstyojaop <bmbdyojaop@gmail.com>
2023-01-28 17:22:16 +0900

    :sparkles: useSliceを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { FC, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectUser, login, logout } from './features/userSlice'
+import { auth } from './firebase'
+const App: FC = () => {
+  // ReduxのStateの中からuserのstateを取得
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    // Firebaseのユーザに対して変化があった際に実行する
+    // この関数を実行するとサブスクライブが始まって、ユーザーの変化の監視を始める
+    // この関数の返り値として、アンサブスクライブするための関数を返り血として返してくれる。
+    // 引数に変化後のユーザの情報が入ってくるので、authUser(変数名は任意)という変数に格納しておく
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      // dispatchでログインのアクションを実行する
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+    // Crean Up関数
+    return () => {
+      unSub()
+    }
+    // 第二引数にはdispatch関数を指定する
+  }, [dispatch])
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
-export default App;
+export default App
